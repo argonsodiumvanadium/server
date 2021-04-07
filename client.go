dc15fb75618d8611f5d1ff3e14e4fad75268d754
arnav rustagi <51513216+argonsodiumvanadium@users.noreply.github.com>
2021-04-07 09:11:03 +0530

    added client.go

--- client.go
+++ client.go
@@ -3,30 +3,31 @@ package main
 import (
 	"log"
 	"net/http"
-	"encoding/json"
 	"fmt"
-	"bytes"
 	"io/ioutil"
+	"bufio"
+	"strings"
+	"os"
 )
 
 const HOST_NAME = "http://localhost:6969/"
 
-func main () {
+func main () {	
+	var ip string
+	
+	fmt.Print("enter IP : ")
+	fmt.Scanln(&ip)
+
+
 	var name string
 	
 	fmt.Print("enter name : ")
 	fmt.Scanln(&name)
+
+	data := map[string][]string{"name": {name}}
+
+	response , err := http.PostForm("http://"+ip+":6969/login",data)
 	
-	reqBody, err := json.Marshal (map[string]string {
-		"name" : name, 
-	})
-
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	response , err := http.Post("http://localhost:6969/login","application/json",bytes.NewBuffer(reqBody))
-
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -36,5 +37,53 @@ func main () {
 	if err != nil {
 		print(err)
 	}
+
 	fmt.Println(string(body))
+
+	go Listen()
+
+	for {
+		fmt.Print(" : ")
+
+		reader := bufio.NewReader(os.Stdin)
+		// ReadString will block until the delimiter is entered
+		msg, err := reader.ReadString('\n')
+		if err != nil {
+			fmt.Println("An error occured while reading input. Please try again", err)
+			return
+		}
+
+		// remove the delimeter from the string
+		msg = strings.TrimSuffix(msg, "\n")
+
+		data := map[string][]string{"message": {msg},"name" : {name}}
+
+		_ , err = http.PostForm("http://localhost:6969/post",data)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+	}
+}
+
+func Listen () {
+	http.HandleFunc ("/broadcast",GetMessage)
+
+	log.Print("\u001B[92mchat joined\u001B[0m")
+	if err := http.ListenAndServe(":4242",nil); err != nil {
+		log.Fatal(err)
+	}
+}
+
+func GetMessage (writer http.ResponseWriter, request *http.Request) {
+	if request.Method == "POST" {
+		if err := request.ParseForm(); err != nil {
+			log.Fatal(err)
+		}
+
+	log.Print(request.PostForm["message"][0])
+
+	} else {
+		panic("only post method is allowed")
+	}
 }
--- server.go
+++ server.go
@@ -3,17 +3,36 @@
 import (
 	"log"
 	"net/http"
+	"math/rand"
+	"strings"
 )
 
 type (
-	MessagePoster struct {
-		
+	LogInManager struct {
+		members []*Member
+		colorMap map[string]*Member
 	}
+
+	Member struct {
+		color string
+		name string
+		URL string
+	}
+)
+
+var (
+	colors = [ ... ]string{"\u001B[91m","\u001B[92m","\u001B[96m","\u001B[95m"}
+	loginManager = &LogInManager{members : make([]*Member,0),colorMap : make(map[string]*Member)}
+)
+
+const (
+	RESET = "\u001B[0m"
 )
 
 func main () {
 	http.HandleFunc ("/post",PostMessage)
-	http.HandleFunc ("/login",Login)
+
+	http.Handle ("/login",loginManager)
 
 	log.Print("\u001B[32mstarting server\u001B[0m")
 	if err := http.ListenAndServe(":6969",nil); err != nil {
@@ -28,23 +47,71 @@ func PostMessage (writer http.ResponseWriter, request *http.Request) {
 			log.Fatal(err)
 		}
 
-	log.Printf( "Post from website! r.PostFrom = %v\n", request.PostForm)
+	msg := request.PostForm["message"][0]
+	name := request.PostForm["name"][0]
+
+	msg = ParseMessage(msg,name)
+
+	log.Println("msg recv",msg)
+
+	for i , member := range loginManager.members {
+		data := map[string][]string{"message": {msg}}
+		_, err := http.PostForm(member.URL,data)
+
+		if err != nil {
+			log.Println("\u001B[91m"+member.name+" left\u001B[0m")
+			loginManager.RemoveMemberAt(i)
+		}
+	}
+
+
 		
 	} else {
 		panic("only post method is allowed")
 	}
 }
 
-func Login (writer http.ResponseWriter, request *http.Request) {
-	log.Print("OK")
+func (self *LogInManager) RemoveMemberAt (i int) {
+	self.members[len(self.members)-1], self.members[i] = self.members[i], self.members[len(self.members)-1]
+	self.members = self.members[:len(self.members)-1]
+}
+
+func ParseMessage (msg,name string) (message string) {
+	color := loginManager.colorMap[name].color
+
+	message = color + name + RESET + " : " + msg + "\n"
+
+	return
+}
+
+func (self *LogInManager) ServeHTTP (writer http.ResponseWriter, request *http.Request) {
+	log.Print("user entered")
 	if request.Method == "POST" {
 		if err := request.ParseForm(); err != nil {
 			log.Fatal(err)
 		}
 	
-	log.Printf( "Post from website! r.PostFrom = %v\n", request.PostForm)
-			
+	newUser := request.PostForm["name"][0]
+
+	writer.Header().Add("Content-Type", "application/json")
+	ip := GetIP(request)
+
+	log.Println(ip)
+
+	member := &Member {color : colors[rand.Intn(len(colors))],name : newUser,URL : "http://" + ip + ":4242/broadcast"}
+
+	self.members = append(self.members, member)
+	self.colorMap[newUser] = member
+
 	} else {
 		panic("only post method is allowed")
 	}
+}
+
+func GetIP(r *http.Request) string {
+	forwarded := r.Header.Get("X-FORWARDED-FOR")
+	if forwarded != "" {
+		return forwarded
+	}
+	return strings.Split(r.RemoteAddr,":")[0]
 }
